7cdfc369dec80e827d1ebe904ba82ea6a6fc4f65
Matheus Silas <54551805+Matheus-Silas97@users.noreply.github.com>
2020-03-20 22:00:37 -0300

    changed spacing of views. Added login activity field validation

--- app/src/main/java/com/example/healthtalk/activity/LoginActivity.java
+++ app/src/main/java/com/example/healthtalk/activity/LoginActivity.java
@@ -3,14 +3,31 @@ package com.example.healthtalk.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.healthtalk.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText txtcpf, txtPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        txtcpf = findViewById(R.id.txtCPF);
+        txtPassword = findViewById(R.id.txtPassword);
+    }
+
+    public void Login() {
+        String cpf = txtcpf.getText().toString();
+        String password = txtPassword.getText().toString();
+        if (cpf.equals("") || cpf == null) {
+            Toast.makeText(getApplicationContext(), "Preencher o campos CPF", Toast.LENGTH_LONG).show();
+        } else if (password.equals("") || password == null) {
+            Toast.makeText(getApplicationContext(), "Preencher o campos Senha", Toast.LENGTH_LONG).show();
+        }
     }
 }
